ccef7da6b60a6f973c459c0bd13a18fca86ad71c
andesome <ling95613@gmail.com>
2020-03-14 23:19:42 +0800

    updload build files

---
+++ build/precache-manifest.71deecd6c01380b002340e347a645c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb01ceb62657b53227c0ead1534782f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d2b2491bfd260e2c12",
+    "url": "/static/css/main.749dcc64.chunk.css"
+  },
+  {
+    "revision": "2ea7c1b0afc84ff1891f",
+    "url": "/static/js/2.4b820380.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4b820380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d2b2491bfd260e2c12",
+    "url": "/static/js/main.331423c9.chunk.js"
+  },
+  {
+    "revision": "7940a55434a4f8d77399",
+    "url": "/static/js/runtime-main.38f19673.js"
+  }
+]);
