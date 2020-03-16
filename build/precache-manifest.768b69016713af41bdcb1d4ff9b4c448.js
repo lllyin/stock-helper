cbd066c3a1538c0603c1f7be5f12731b5e0110e9
andesome <ling95613@gmail.com>
2020-03-16 13:49:43 +0800

    build files

---
+++ build/precache-manifest.768b69016713af41bdcb1d4ff9b4c448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c996a01b80b03d301724e0e17cd0de00",
+    "url": "/stock-helper/build/index.html"
+  },
+  {
+    "revision": "103f1bd075c59ac7976e",
+    "url": "/stock-helper/build/static/css/main.9eda70a6.chunk.css"
+  },
+  {
+    "revision": "a344f2a6defa802fad4d",
+    "url": "/stock-helper/build/static/js/2.b636b481.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stock-helper/build/static/js/2.b636b481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103f1bd075c59ac7976e",
+    "url": "/stock-helper/build/static/js/main.cfe308f7.chunk.js"
+  },
+  {
+    "revision": "31759f6d4f19dbc73c19",
+    "url": "/stock-helper/build/static/js/runtime-main.c8110caa.js"
+  },
+  {
+    "revision": "47681e146c817df03a9d7cc82669b81a",
+    "url": "/stock-helper/build/static/media/edit-icon.47681e14.svg"
+  }
+]);
