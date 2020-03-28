c9d28ea11500a8612dd1f4d8021b339c8714dd13
andesome <ling95613@gmail.com>
2020-03-28 10:02:03 +0800

    upload

---
+++ build/precache-manifest.f58d8dc493535879a864dce0d817bd82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf92c32b9c6ef80333271c3d0e82f9a",
+    "url": "/stock-helper/build/index.html"
+  },
+  {
+    "revision": "fdd3730a67053ac1d684",
+    "url": "/stock-helper/build/static/css/main.13914974.chunk.css"
+  },
+  {
+    "revision": "17cfeb983eaf424b1e24",
+    "url": "/stock-helper/build/static/js/2.2cad79f1.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/stock-helper/build/static/js/2.2cad79f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd3730a67053ac1d684",
+    "url": "/stock-helper/build/static/js/main.758ea9bf.chunk.js"
+  },
+  {
+    "revision": "31759f6d4f19dbc73c19",
+    "url": "/stock-helper/build/static/js/runtime-main.c8110caa.js"
+  },
+  {
+    "revision": "4f995bf33c821b6fded796a3c6cc36b6",
+    "url": "/stock-helper/build/static/media/add-icon.4f995bf3.svg"
+  },
+  {
+    "revision": "7d8cde5eedd4c46a55964fd485c7e518",
+    "url": "/stock-helper/build/static/media/delete-icon.7d8cde5e.svg"
+  },
+  {
+    "revision": "47681e146c817df03a9d7cc82669b81a",
+    "url": "/stock-helper/build/static/media/edit-icon.47681e14.svg"
+  },
+  {
+    "revision": "c5e730eadf57f3e5cb1b5e291480f209",
+    "url": "/stock-helper/build/static/media/save-icon.c5e730ea.svg"
+  }
+]);
